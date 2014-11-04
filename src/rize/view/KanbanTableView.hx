29e14a32ff3193266b5359bb48446b7b379f46c8
大村響一 <kyos1704@oomurakyouichi-no-MacBook-Air.local>
2014-11-04 16:28:59 +0900

    add html

--- src/rize/view/KanbanTableView.hx
+++ src/rize/view/KanbanTableView.hx
@@ -1,4 +1,10 @@
 package rize.view;
 
-@:build(mage.CompileHTML.generate(""))
+@:build(mage.CompileHTML.generate(
+"package rize.view
+
+<div>
+	<p>{{text}}</p>
+</div>"
+))
 class KanbanTableView{}
