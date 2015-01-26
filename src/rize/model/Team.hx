51a028e45b8f7079acca5d67e02aae071eba9b53
Nobukazu Hanada <tone.nobukazu@gmail.com>
2015-01-26 20:11:17 +0900

    add Team class

--- src/rize/model/Team.hx
+++ src/rize/model/Team.hx
@@ -1,9 +1,10 @@
 package rize.model.Team;
 
 class Team{
+	public var members : Array<Developer>;
 
 	public function new(){
-
+		
 	}
 
 	public function add(){
@@ -11,6 +12,6 @@ class Team{
 	}
 
 	public function search(){
-		
+
 	}
 }
